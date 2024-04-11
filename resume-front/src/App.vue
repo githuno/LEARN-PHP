a6a7a3d3d59334c29dafe064337b8904904f6e62
githuno <103876809+githuno@users.noreply.github.com>
2024-04-11 07:06:21 +0000

    送信成功

--- resume-front/src/App.vue
+++ resume-front/src/App.vue
@@ -1,26 +1,122 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container py-5">
+		<form @submit.prevent="submitForm" class="needs-validation" novalidate>
+			<InputField id="name" label="名前" v-model="form.name" required />
+			<InputField id="kana" label="フリガナ" v-model="form.kana" />
+			<InputField id="birthDate" label="生年月日" v-model="form.birthDate" type="date" />
+			<InputField id="postalCode" label="郵便番号" v-model="form.postalCode" />
+			<InputField id="address" label="現住所" v-model="form.address" />
+			<div class="form-group mt-3">
+				<label class="form-label">性別:</label>
+				<div class="d-flex">
+					<div class="form-check mr-3">
+						<input class="form-check-input" id="male" v-model="form.gender" type="radio" value="男" />
+						<label class="form-check-label" for="male">男</label>
+					</div>
+					<div class="form-check mr-3">
+						<input class="form-check-input" id="female" v-model="form.gender" type="radio" value="女" />
+						<label class="form-check-label" for="female">女</label>
+					</div>
+					<div class="form-check">
+						<input class="form-check-input" id="other" v-model="form.gender" type="radio" value="その他" />
+						<label class="form-check-label" for="other">その他</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group mt-3">
+				<label for="phone" class="form-label">電話番号:</label>
+				<input id="phone" v-model="form.phone" type="tel" class="form-control" />
+			</div>
+			<div class="form-group mt-3">
+				<label for="email" class="form-label">メールアドレス:</label>
+				<input id="email" v-model="form.email" type="email" class="form-control" />
+			</div>
+			<div class="form-group mt-3">
+				<label for="motivation" class="form-label">志望動機:</label>
+				<textarea id="motivation" v-model="form.motivation" class="form-control"></textarea>
+			</div>
+			<div class="form-group mt-3">
+				<label for="selfPr" class="form-label">自己PR:</label>
+				<textarea id="selfPr" v-model="form.selfPr" class="form-control"></textarea>
+			</div>
+			<div v-for="(experience, index) in form.experiences" :key="index"
+				class="flex-column align-items-center mt-3">
+				<div class="d-flex align-items-center justify-content-between">
+					<label class="form-label mr-3">資格・経験{{ index + 1 }}</label>
+					<button type="button" class="btn btn-danger ml-3" @click="removeExperience(index)">
+						<i class="bi bi-dash"></i>
+					</button>
+				</div>
+				<UserExperience :experience="experience" @update="updateExperience(index, $event)" class="flex-grow" />
+			</div>
+			<div class="d-flex justify-content-end">
+				<button type="button" class="btn btn-primary mt-3" @click="addExperience">
+					<i class="bi bi-plus"></i>
+				</button>
+			</div>
+			<button type="submit" class="btn btn-primary mt-3">作成</button>
+		</form>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import UserExperience from './components/UserExperience.vue';
+import InputField from './components/InputField.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	components: {
+		UserExperience,
+		InputField,
+	},
+	data() {
+		return {
+			form: {
+				name: '',
+				kana: '',
+				birthDate: '',
+				postalCode: '',
+				address: '',
+				gender: '',
+				phone: '',
+				email: '',
+				motivation: '',
+				experiences: [
+					{
+						date: '',
+						industry: '',
+						description: '',
+						tools: '',
+					},
+				],
+			}
+		}
+	},
+	methods: {
+		async submitForm() {
+			try {
+				// POSTリクエストを送信
+				const response = await axios.post('https://sturdy-spork-p54pw4wvjgr397p9-8000.app.github.dev/resume', this.form, {
+				});
+				console.log(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		addExperience() {
+			this.form.experiences.push({
+				date: '',
+				industry: '',
+				description: '',
+				tools: '',
+			});
+		},
+		removeExperience(index) {
+			this.form.experiences.splice(index, 1);
+		},
+        updateExperience(index, updatedExperience) {
+            this.form.experiences[index] = updatedExperience;
+        },
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
